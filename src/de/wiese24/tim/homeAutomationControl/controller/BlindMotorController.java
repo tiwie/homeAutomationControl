0d6ff52a2b6388c2d522d768f50874efac96f802
Stefan Wiese <wiesestefan@web.de>
2013-11-17 21:29:06 +0100

    some minor changes for controller logic

--- src/de/wiese24/tim/homeAutomationControl/controller/BlindMotorController.java
+++ src/de/wiese24/tim/homeAutomationControl/controller/BlindMotorController.java
@@ -1,5 +1,6 @@
 package de.wiese24.tim.homeAutomationControl.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import de.wiese24.tim.homeAutomationControl.actors.Actor;
@@ -8,11 +9,14 @@ import de.wiese24.tim.homeAutomationControl.actors.actions.BlindMotorAction;
 import de.wiese24.tim.homeAutomationControl.actors.actions.BlindMotorActionValue;
 import de.wiese24.tim.homeAutomationControl.sensors.SensorState;
 import de.wiese24.tim.homeAutomationControl.sensors.SensorType;
+import de.wiese24.tim.homeAutomationControl.sensors.states.Temperature;
 import de.wiese24.tim.homeAutomationControl.sensors.states.WindSpeed;
 
 public class BlindMotorController extends ActorController {
 
 	private WindSpeed oldWindSpeed;
+
+	private Temperature oldTemperature;
 
 	@Override
 	public void handleSensorStates(List<SensorState> states) {
@@ -25,20 +29,48 @@ public class BlindMotorController extends ActorController {
 	private ActorAction createAction(List<SensorState> states) {
 		for (SensorState state : states) {
 			SensorType type = state.getSensorType();
+			ActorAction windSpeedAction = null;
+			ActorAction temperatureAction = null;
+			ActorAction timeAction = null;
 			switch (type) {
 			case WIND_SPEED:
-				int value = ((Integer) state.getValue()).intValue();
-				if (value > 20) {
+				int windSpeed = ((Integer) state.getValue()).intValue();
+				if (windSpeed == ((Integer) oldWindSpeed.getValue()).intValue()) {
+					break;
+				}
+				if (windSpeed > 20) {
 					return new BlindMotorAction(BlindMotorActionValue.CLOSE);
+				} else {
+					windSpeedAction = new BlindMotorAction(
+							BlindMotorActionValue.OPEN);
 				}
 				break;
 			case TEMPERATURE:
+				int temperature = ((Integer) state.getValue()).intValue();
+				if (temperature == ((Integer) oldTemperature.getValue())
+						.intValue()) {
+					break;
+				}
+				if (temperature > 20) {
+					temperatureAction = new BlindMotorAction(
+							BlindMotorActionValue.CLOSE);
+				} else if (temperature < 20 && temperature > 15) {
+					temperatureAction = new BlindMotorAction(
+							BlindMotorActionValue.DIM_OFF);
+				} else {
+					temperatureAction = new BlindMotorAction(
+							BlindMotorActionValue.OPEN);
+				}
 				break;
 			case TIME:
+				Date date = (Date) state.getValue();
+				// finde heraus, ob es nach 06:00 a.m. Uhr ist oder nach 6:00
+				// p.m.
 				break;
 			default:
 				break;
 			}
+
 		}
 		return null;
 	}
